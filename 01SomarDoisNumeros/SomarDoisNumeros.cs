51da9428a44eb6e608907336ff3946bad33705c5
Bernardo <bernardoaquinoc@gmail.com>
2022-02-04 19:36:11 -0300

    First commit

---
+++ 01SomarDoisNumeros/SomarDoisNumeros.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01SomarDoisNumeros
+{
+    class SomarDoisNumeros
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, soma;
+
+            Console.WriteLine("Digite um número:");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número:");
+            num2 = int.Parse(Console.ReadLine());
+
+            soma = num1 + num2;
+
+            Console.WriteLine("Soma: " + soma);
+        }
+    }
+}
